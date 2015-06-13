d1868b74f4ecd85b66fdc351ec5c0f677b819024
Livi <livi@outlook.com>
2015-06-12 17:25:17 -0700

    Added effects to load screen

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectWithTag ("mainPiano").GetComponentInChildren<Text> ().text = "Piano";
+		GameObject.FindGameObjectWithTag ("mainChaos").GetComponentInChildren<Text> ().text = "Chaos";
+		GameObject.FindGameObjectWithTag ("mainAbout").GetComponentInChildren<Text> ().text = "About";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// We want to load in the scene for the piano tones
+	public void LoadPiano(){
+	}
+
+	// Load in Chaos scene
+	public void LoadChaos(){
+		Application.LoadLevel ("Synethesia");
+	}
+	
+}
